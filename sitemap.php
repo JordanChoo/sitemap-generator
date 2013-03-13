acfc474140febfd021859aa605a6f2382d8dc3b3
Jordan Choo <jordan@jordanchoo.com>
2013-03-12 20:21:00 -0400

    Fixed current sitemap method so it now grabs the name of the sitemap the OOP way

--- sitemap.php
+++ sitemap.php
@@ -13,7 +13,7 @@ class GenericSitemap
 		$this->sitemap_uri = $sitemap_folder."/".$sitemap_name;
 	}
 
-	function sitemap($config)
+	public function sitemap($config)
 	{
 		$url = $config['params']['loc'];
 		// Check if the sitemap folder exists
@@ -22,9 +22,9 @@ class GenericSitemap
 			// Create a sitemap folder
 			mkdir($this->sitemap_folder) or die('Unable to create sitemap folder: '.$this->sitemap_folder);
 		}
-		$sitemap_name = $this->current_sitemap($sitemap_name, $this->sitemap_folder);
+		$this->sitemap_name = $this->current_sitemap();
 		// Check if the sitemap exists
-		if(!file_exists($this->sitemap_uri".xml"))
+		if(!file_exists($this->sitemap_uri.".xml"))
 		{
 			// Create the sitemap
 			$this->create_sitemap($this->sitemap_uri);
@@ -36,7 +36,7 @@ class GenericSitemap
 		// Count the nodes
 		if($node_count >= 50000)
 		{
-			$sitemap_name = $this->increment_sitemap($config['sitemap_name'], $this->sitemap_folder);
+			$this->sitemap_name = $this->increment_sitemap();
 			// // Create the sitemap
 			$this->create_sitemap($this->sitemap_folder.'/'.$sitemap_name) or die("Unable to create the sitemap");
 		}
@@ -45,11 +45,11 @@ class GenericSitemap
 	}
 
 	// Create sitemap function
-	private function create_sitemap($sitemap_name)
+	private function create_sitemap()
 	{
-		$sitemap_name .= ".xml";
+		$this->sitemap_name .= ".xml";
 		// Create the sitemap
-		$open_map = fopen($sitemap_name, "w");
+		$open_map = fopen($this->sitemap_name, "w");
 		if(!$open_map) 
 		{
 			// if there was an error
@@ -60,16 +60,16 @@ class GenericSitemap
 		// Create the "empty" sitemap
 		$create_xml = new SimpleXMLElement('<urlset></urlset>');
 		$create_xml->addAttribute("xmlns", "http://www.sitemaps.org/schemas/sitemap/0.9");
-		$create_xml->asXML($sitemap_name);
+		$create_xml->asXML($this->sitemap_name);
 		// Show that it was success
 		return true;
 	}
 
 
-	private function current_sitemap($sitemap_name, $sitemap_folder, $offset = 0)
+	private function current_sitemap($offset = 0)
 	{
 		// count the number of sitemaps in the folder with the same name
-		$folder_data = opendir($sitemap_folder);
+		$folder_data = opendir($this->sitemap_folder);
 		// create an empty array
 		$sitemap_folder_content = array();
 		while (false !== ($entry = readdir($folder_data))) {
@@ -77,7 +77,7 @@ class GenericSitemap
 	    }
 		foreach ($sitemap_folder_content as $sitemap) 
 		{
-			$result = strpos($sitemap, $sitemap_name);
+			$result = strpos($sitemap, $this->sitemap_name);
 			if($result !== false)
 			{
 				$offset++;	
@@ -88,18 +88,18 @@ class GenericSitemap
 			$offset++;
 		}
 
-		return $sitemap_name."_".$offset;
+		return $this->sitemap_name."_".$offset;
 	}
 
 
-	private function increment_sitemap($sitemap_name, $sitemap_folder, $offset = 1)
+	private function increment_sitemap($offset = 1)
 	{
-		$current = $this->current_sitemap($sitemap_name, $sitemap_folder);
+		$current = $this->current_sitemap($this->sitemap_name, $this->sitemap_folder);
 
 		preg_match('/(.+)'.'_'.'([0-9]+)$/', $current, $match);
-		$sitemap_name =  isset($match[2]) ? $match[1].'_'.($match[2] + 1) : $sitemap_name.'_'.$offset;
+		$this->sitemap_name =  isset($match[2]) ? $match[1].'_'.($match[2] + 1) : $this->sitemap_name.'_'.$offset;
 
-		return $sitemap_name;
+		return $this->sitemap_name;
 	}
 
 	// Make writing the actual file a new function
